2764ed9d31b0b67041f9d07f927359ac0f8c0bc5
Leo Gray <leograyc@gmail.com>
2019-03-19 17:12:03 -0400

    setup the new parit of files for ADC sampling

---
+++ Lab1/sampler.c
@@ -0,0 +1,31 @@
+/*
+ * sampler.c
+ *
+ *  Created on: 2019-3-19
+ *      Author: Leo
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/adc.h"
+#include "sysctl_pll.h"
+
+#include "sampler.h"
+
+void ADCInit()
+{
+
+
+}
+
+
+
+
+
+
---
+++ Lab1/sampler.h
@@ -0,0 +1,13 @@
+/*
+ * Sampler.h
+ *
+ *  Created on: 2019-3-19
+ *      Author: Leo
+ */
+
+#ifndef SAMPLER_H_
+#define SAMPLER_H_
+
+
+
+#endif /* SAMPLER_H_ */
